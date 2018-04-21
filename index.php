46fb937ac8049ebd644fe7639ed231b1b3163e39
Niko Barateli <nbara15@freeuni.edu.ge>
2018-04-21 13:53:25 +0400

    favicon

--- index.php
+++ index.php
@@ -17,21 +17,25 @@ include_once "utils.php"
   <link rel="shortcut icon" href="favicon.ico"/>
 
   <link rel="stylesheet" href="css/main.css">
-  <!--  <link rel="stylesheet" href="css/normalize.css">-->
+  <!--    <link rel="stylesheet" href="css/normalize.css">-->
 </head>
 <body>
 <!--[if lte IE 9]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a
+        href="https://browsehappy.com/">upgrade
   your browser</a> to improve your experience and security.</p>
 <![endif]-->
 <div id="header">
-  <img src="img/reddit_logo.png">
-  <ul class="menu">
-    <li class="menu-item"><a class="menu-link menu-item-selected" href="#"><b>Niko</b></a></li>
-    <li class="menu-item"><a class="menu-link" href="#"><b>Nikora</b></a></li>
-    <li class="menu-item"><a class="menu-link" href="#"><b>Statusebi</b></a></li>
-    <li class="menu-item"><a class="menu-link" href="#"><b>Mee</b></a></li>
-  </ul>
+  <div class="header-left">
+    <img src="img/reddit_logo.png">
+    <ul class="menu">
+      <li class="menu-item"><a class="menu-link menu-item-selected" href="#"><b>Niko</b></a></li>
+      <li class="menu-item"><a class="menu-link" href="#"><b>Nikora</b></a></li>
+      <li class="menu-item"><a class="menu-link" href="#"><b>Statusebi</b></a></li>
+      <li class="menu-item"><a class="menu-link" href="#"><b>Mee</b></a></li>
+    </ul>
+  </div>
+
   <div id="header-right">
     <div id="userspace">
       <ul>
@@ -61,101 +65,103 @@ include_once "utils.php"
     </div>
   </div>
 </div>
+<div id="wrapper">
+  <div id="content">
+    <p>Hello world! This is HTML5 Boilerplate.</p>
+      <?php
+      $content = file_get_contents("database/reddit-posts.json");
+      $reddit = json_decode($content);
+      usort($reddit, function ($a, $b) {
 
-<div id="side" class="side">
-  <div id="customize" class="side">
-    <h1>Customize Your Reddit</h1>
-    <ul id="categories" class="filters">
-        <?php
-        $communities = getCommunityList();
+          return $b->up - $a->up;
+      });
+      $i = 0;
+      foreach ($reddit as $post) {
+//        $post = new Entry();
+          ?>
+        <div class="post">
+          <div class="rank"><?= ++$i ?></div>
+          <div class="vote">
+            <div class="vote-arrow vote-up"></div>
+            <div class="vote-value"><b><?= countVote($post->up) ?> </b></div>
+            <div class="vote-arrow vote-down"></div>
+          </div>
+          <div class="post-image">
+            <img src="<?= getPicture($post) ?>">
+          </div>
+          <div class="post-content">
+            <div class="title">
+              <h3><a href="<?= $post->url ?>"><?= $post->title ?></a>
+                <small class="source">(<?= getWebSite($post->url) ?>)</small>
+              </h3>
+            </div>
+            <div class="details">
+              <small>submitted 21 minutes ago by <a
+                        href="https://reddit.com/user<?= $post->author ?> ">
+                      <?= $post->author ?>
+                </a> tagged: <?= renderTags($post->tags) ?>
+              </small>
+              <br>
+              <small><b> 218 comments save hide report </b></small>
+            </div>
+          </div>
+        </div>
+          <?php
+      } ?>
+  </div>
 
-        foreach ($communities as $community) {
-            ?>
-          <li>
-            <label class="container"> <?= $community ?>
-              <input type="checkbox" checked="checked">
-              <span class="checkmark"></span>
-            </label>
-          </li>
-            <?php
-        }
-        ?>
-    </ul>
+  <div id="side" class="side">
+    <div id="customize" class="side">
+      <h1>Customize Your Reddit</h1>
+      <ul id="categories" class="filters">
+          <?php
+          $communities = getCommunityList();
 
-    <div id="popular-tags">
-      <h1>Popular Tags</h1>
-        <?php
-        $tags = getTags(json_decode(file_get_contents('database/reddit-posts.json')));
-        foreach ($tags as $tag) {
-            if ($tag['count'] < 2 && rand(0, 1) == 1) continue;
-            ?>
-          <span class="<?= getRank($tag['z']) ?>"><?= $tag['tag'] ?></span>
-            <?php
-        }
-        ?>
-    </div>
-    <div id="submit" class="side">
-      <div class="logo"><img class="logo" src="img/reddit_submit.png"></div>
-      <div class="side-content">
-        <a href="#"><h4>Submit a link >></h4></a><br>
-        to anything interesting: news article, blog, entry, video, picture...
+          foreach ($communities as $community) {
+              ?>
+            <li>
+              <label class="container"> <?= $community ?>
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+              </label>
+            </li>
+              <?php
+          }
+          ?>
+      </ul>
+
+      <div id="popular-tags">
+        <h1>Popular Tags</h1>
+          <?php
+          $tags = getTags(json_decode(file_get_contents('database/reddit-posts.json')));
+          foreach ($tags as $tag) {
+              if ($tag['count'] < 2 && rand(0, 1) == 1) continue;
+              ?>
+            <span class="<?= getRank($tag['z']) ?>"><?= $tag['tag'] ?></span>
+              <?php
+          }
+          ?>
       </div>
-    </div>
-    <div id="create" class="side">
-      <div class="logo"><img class="logo" src="img/reddit_create.png"></div>
-      <div class="side-content">
-        <a href="#"><h4>Create your own reddit >></h4></a>
-        <br>
-        ...because you love freedom.
-        <br>
-        ...for your WoW guild.
+      <div id="submit" class="side">
+        <div class="logo"><img class="logo" src="img/reddit_submit.png"></div>
+        <div class="side-content">
+          <a href="#"><h4>Submit a link >></h4></a><br>
+          to anything interesting: news article, blog, entry, video, picture...
+        </div>
+      </div>
+      <div id="create" class="side">
+        <div class="logo"><img class="logo" src="img/reddit_create.png"></div>
+        <div class="side-content">
+          <a href="#"><h4>Create your own reddit >></h4></a>
+          <br>
+          ...because you love freedom.
+          <br>
+          ...for your WoW guild.
+        </div>
       </div>
     </div>
   </div>
-</div>
-<!-- Add your site or application content here -->
-<div id="content">
-  <p>Hello world! This is HTML5 Boilerplate.</p>
-    <?php
-    $content = file_get_contents("database/reddit-posts.json");
-    $reddit = json_decode($content);
-    usort($reddit, function ($a, $b) {
-
-        return $b->up - $a->up;
-    });
-    $i = 0;
-    foreach ($reddit as $post) {
-//        $post = new Entry();
-        ?>
-      <div class="post">
-        <div class="rank"><?= ++$i ?></div>
-        <div class="vote">
-          <div class="vote-arrow vote-up"></div>
-          <div class="vote-value"><b><?= countVote($post->up) ?> </b></div>
-          <div class="vote-arrow vote-down"></div>
-        </div>
-        <div class="post-image">
-          <img src="<?= getPicture($post) ?>">
-        </div>
-        <div class="post-content">
-          <div class="title">
-            <h3><a href="/"><?= $post->title ?></a>
-              <small class="source">(<?= getWebSite($post->url) ?>)</small>
-            </h3>
-          </div>
-          <div class="details">
-            <small>submitted 21 minutes ago by <a
-                      href="https://reddit.com/user<?= $post->author ?> ">
-                    <?= $post->author ?>
-              </a> tagged: <?= renderTags($post->tags) ?>
-            </small>
-            <br>
-            <small><b> 218 comments save hide report </b></small>
-          </div>
-        </div>
-      </div>
-        <?php
-    } ?>
+  <!-- Add your site or application content here -->
 </div>
 <script src="js/vendor/modernizr-3.5.0.min.js"></script>
 <script src="js/vendor/jquery-3.2.1.min.js"
